1a880ec81deb8bac5dcae930e4a38a29a1c92cdd
Egor <koropeczegor@gmail.com>
2024-03-27 12:32:26 +0300

    Structure

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,10 +1,30 @@
 import React from 'react';
+import reportWebVitals from './reportWebVitals';
 import {createRoot} from "react-dom/client"
 import { Provider } from 'react-redux';
 import { store } from './app/store';
-import reportWebVitals from './reportWebVitals';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { Paths } from './paths';
+import { Login } from './pages/login';
+import { Register } from './pages/register';
+
 import "./index.css"
 
+
+const router = createBrowserRouter([
+  {
+    path: Paths.home,
+    element: <h1>Home</h1>
+  },
+  {
+    path: Paths.login,
+    element: <Login/>
+  },
+  {
+    path: Paths.register,
+    element: <Register/>
+  }
+])
 const container = document.getElementById("root")!;
 const root = createRoot(container);
 
@@ -12,7 +32,7 @@ const root = createRoot(container);
   root.render(
     <React.StrictMode>
     <Provider store={store}>
-      <h1>Hello world</h1>
+      <RouterProvider router={ router}/>
     </Provider>
     </React.StrictMode>
   );
