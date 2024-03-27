cc30c197f5d473a0ca13be1c70a5f2b600a66cc0
Egor <koropeczegor@gmail.com>
2024-03-27 21:54:28 +0300

    specify the night theme

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -7,6 +7,7 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import { Paths } from './paths';
 import { Login } from './pages/login';
 import { Register } from './pages/register';
+import { ConfigProvider, theme } from 'antd';
 
 import "./index.css"
 
@@ -32,7 +33,11 @@ const root = createRoot(container);
   root.render(
     <React.StrictMode>
     <Provider store={store}>
-      <RouterProvider router={ router}/>
+      <ConfigProvider theme={{
+        algorithm: theme.darkAlgorithm
+      }}>
+        <RouterProvider router={ router}/>
+      </ConfigProvider>
     </Provider>
     </React.StrictMode>
   );
