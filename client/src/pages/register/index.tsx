cdff7efbba86c753a76f598a1bdcf2e7a2d68d1c
Egor <koropeczegor@gmail.com>
2024-03-31 19:23:20 +0300

    Register

--- client/src/pages/register/index.tsx
+++ client/src/pages/register/index.tsx
@@ -1,7 +1,33 @@
+import { Card, Form, Row, Space, Typography } from 'antd'
+import { Layout } from '../../components/layout'
 import React from 'react'
+import { CustomInput } from '../../components/custom-input'
+import { CustomPasswordInput } from '../../components/custom-password-input'
+import { CustomButton } from '../../components/custom-button'
+import { Link } from 'react-router-dom'
+import { Paths } from '../../paths'
 
 export const Register = () => {
   return (
-    <div>Register</div>
+    <Layout>
+    <Row align="middle" justify="center">
+      <Card title="Зарегистрируйтесь" style={{width: "30rem"}}>
+          <Form onFinish={() => null}>
+            <CustomInput type="text" name='name' placeholder='Имя'/>
+            <CustomInput type="email" name='email' placeholder='Email'/>
+            <CustomPasswordInput name='password' placeholder='Пароль'/>
+            <CustomPasswordInput name='canfirmPassword' placeholder='Повторите пароль'/>
+            <CustomButton type='primary' htmlType='submit'>
+            Зарегистрируйтесь
+            </CustomButton>
+          </Form>
+          <Space direction='vertical' size="large">
+            <Typography.Text>
+              Уже зарегистрированы? <Link to={ Paths.login }>Войдите</Link>
+            </Typography.Text>
+          </Space>
+      </Card>
+    </Row>
+</Layout>
   )
 }
