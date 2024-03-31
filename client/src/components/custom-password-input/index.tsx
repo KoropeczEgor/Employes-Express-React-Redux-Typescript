d2a75e48566841eba11ddc37585081ba64e2695d
Egor <koropeczegor@gmail.com>
2024-03-31 18:37:39 +0300

    Validation for CustomPasswordInput from And Design

--- client/src/components/custom-password-input/index.tsx
+++ client/src/components/custom-password-input/index.tsx
@@ -18,7 +18,27 @@ export const CustomPasswordInput = ({
     name={name}
     dependencies={dependencies}
     hasFeedback={true}
-    rules={[{required: true, message: 'Обязательное поле'}]}
+    rules={[{required: true, message: 'Обязательное поле'},
+    //Взято из And Design FORM "passworld"
+    ({getFieldValue}) => ({
+        validator(_, value) {
+            if(!value) {
+                return Promise.resolve()
+            }
+
+            if(name === 'confirmPassword') {
+                if(!value || getFieldValue(('password')) === value) {
+                    return Promise.resolve()
+                }
+                
+                return Promise.reject(new Error('Пароль не совподают'))
+            } else {
+                if(value.length < 6) {
+                    return Promise.reject(new Error('Пароль должен быть длиннее 6-ти символов'))
+                }
+            }
+        }
+    })]}
     >
         <Input.Password
         placeholder={placeholder}
