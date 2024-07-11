c3c9f400fdaf60549b8b9a17dfee076029b3d266
Egor <koropeczegor@gmail.com>
2024-07-11 22:14:52 +0300

    feat: additional links to Paths

--- client/src/paths.ts
+++ client/src/paths.ts
@@ -1,10 +1,13 @@
 export const Paths = {
-    home: '/',
-    empolyeeAdd: '/employee/add',
-    employeeEdit: '/employee/edit', 
-    employee: "/employee",
-    status: "/status",
-    login: '/login',
-    register: "/register"
+  home: "/",
+  empolyeeAdd: "/employee/add",
+  employeeEdit: "/employee/edit",
+  employee: "/employee",
+  EmployeeFormeAdd: "/EmployeeForme/add",
+  EmployeeFormeEdit: "/EmployeeForme/edit",
+  EmployeeForm: "/EmployeeForme",
+  status: "/status",
+  login: "/login",
+  register: "/register",
 } as const;
 //as const позволяет отображать пути при наведении на paths
