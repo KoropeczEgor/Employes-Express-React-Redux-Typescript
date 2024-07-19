764e24048ceba088726a9d83e776c6b30c28e6af
Egor <koropeczegor@gmail.com>
2024-07-19 21:52:29 +0300

    feat: creat "store"

--- client/src/app/store.ts
+++ client/src/app/store.ts
@@ -1,9 +1,21 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import counterReduce from '../features/counter/counterSlice';
+import { api } from './services/api';
+import auth from '../features/auth/authSlice';
+import employees from '../features/employees/employeesSlice';
+import { listenerMiddleware } from '../middleware/auth';
 
 export const store = configureStore({
   reducer: {
-   
+    counter: counterReduce,
+    [api.reducerPath]: api.reducer,
+    auth,
+    employees,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware()
+      .concat(api.middleware)
+      .prepend(listenerMiddleware.middleware),
 });
 
 export type AppDispatch = typeof store.dispatch;
