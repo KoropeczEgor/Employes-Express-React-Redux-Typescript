e75a4b6739d9efc8386da34e55eb13ecc2e5bbf7
Egor <koropeczegor@gmail.com>
2024-07-22 20:51:19 +0300

    feat: creat fix "header"

--- client/src/components/header/index.tsx
+++ client/src/components/header/index.tsx
@@ -1,42 +1,61 @@
-import React from 'react'
-import { Layout, Space, Typography } from 'antd'
-import { LoginOutlined, TeamOutlined, UserOutlined } from '@ant-design/icons'
-import { CustomButton } from '../custom-button'
-import { Link } from 'react-router-dom'
-import { Paths } from '../../paths'
+import React from 'react';
+import { Layout, Space, Typography } from 'antd';
+import {
+  LoginOutlined,
+  TeamOutlined,
+  UserOutlined,
+  LogoutOutlined,
+} from '@ant-design/icons';
+import { CustomButton } from '../custom-button';
+import { Link, useNavigate } from 'react-router-dom';
+import { Paths } from '../../paths';
 
-
-import styles from "./index.module.css"
+import styles from './index.module.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { logout, selectUser } from '../../features/auth/authSlice';
 
 export const Header = () => {
+  const user = useSelector(selectUser);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const onLogoutClick = () => {
+    dispatch(logout());
+    localStorage.removeItem('token');
+    navigate('/login');
+  };
   return (
     <Layout.Header className={styles.header}>
       <Space>
-        <TeamOutlined className={styles.teamIcons}/>
-        <Link to={Paths.home}>
-        <CustomButton 
-        type='link' 
+        <TeamOutlined className={styles.teamIcons} />
+        <Link to='/'>
+          <CustomButton type='link'>
+            <Typography.Title level={1}>Сотридников</Typography.Title>
+          </CustomButton>
+        </Link>
+      </Space>
+      {user ? (
+        <CustomButton
+          type='link'
+          icon={<LogoutOutlined />}
+          onClick={onLogoutClick}
         >
-          <Typography.Title level={1}>
-            Сотридников
-          </Typography.Title>
-          </CustomButton>
-        </Link>
-      </Space>
-      {/* //Space это расстояние между  */}
-      <Space>
-        <Link to={Paths.register}>
-          <CustomButton type="primary" icon={<LoginOutlined/>}>
-            Зарегистрировать
-          </CustomButton>
-        </Link>
-        <Link to={Paths.login}>
-          <CustomButton type="primary" icon={<UserOutlined/>}>
-            Войти
-          </CustomButton>
-        </Link>
-      </Space>
+          Выйти
+        </CustomButton>
+      ) : (
+        <Space>
+          <Link to='/register'>
+            <CustomButton type='link' icon={<UserOutlined />}>
+              Зарегистрироваться
+            </CustomButton>
+          </Link>
+          <Link to='/login'>
+            <CustomButton type='link' icon={<LoginOutlined />}>
+              Войти
+            </CustomButton>
+          </Link>
+        </Space>
+      )}
     </Layout.Header>
-    
-  )
-}
+  );
+};
